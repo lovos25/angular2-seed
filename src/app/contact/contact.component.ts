4e4f2989fdde8a486212415727bdce48749927e0
Amanpreet Kaur <amanpreet.kaur@emsgmbh.com>
2017-04-05 10:36:27 +0200

    contact - submit the form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,22 +3,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import CustomValidators from '../forms/CustomValidators';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact-component.css']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact-component.css']
 })
 export class ContactComponent implements OnInit {
-  contactForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+	contactForm: FormGroup;
+	submitted = false;
+	constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
-    this.contactForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, CustomValidators.validateEmail]],
-      content: ['', [Validators.required, Validators.minLength(10)]]
-    });
-  }
-  submitForm(): void {
-    console.log(this.contactForm);
-  }
+	ngOnInit() {
+		this.contactForm = this.formBuilder.group({
+			name: ['', Validators.required],
+			vorname: ['', Validators.required],
+			email: ['', [Validators.required, CustomValidators.validateEmail]],
+			content: ['', [Validators.required, Validators.minLength(10)]]
+		});
+	}
+
+	submitForm(): void {
+		console.log(this.contactForm);
+		if(this.contactForm.valid)
+			this.submitted = true;
+	}
 }
